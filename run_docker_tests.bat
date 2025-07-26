22241f3b8f01f7bd466c30ee0e43e8ce115ec84b
Dogukan686 <dogukanleloglu@gmail.com>
2025-07-27 01:16:29 +0300

    Update run_docker_tests.bat

--- run_docker_tests.bat
+++ run_docker_tests.bat
@@ -1,27 +1,17 @@
 @echo off
-echo --------------------------------------------
-echo Test Automation Project - Docker Build & Run
-echo --------------------------------------------
+echo ---------------------------------------------
+echo Test Automation Project - Docker Build
+echo ---------------------------------------------
 
-:: Docker image build (buildx destekli ve local yükleme)
 docker buildx build --load -t test-automation .
 
-IF %ERRORLEVEL% NEQ 0 (
-    echo [HATA] Docker image olusturulamadi.
-    pause
-    exit /b %ERRORLEVEL%
-)
+echo ---------------------------------------------
+echo Starting Test Container...
+echo ---------------------------------------------
 
-:: Docker container çalıştır
 docker run --rm test-automation
 
-IF %ERRORLEVEL% NEQ 0 (
-    echo [HATA] Docker container icinde testler basarisiz.
-    pause
-    exit /b %ERRORLEVEL%
-)
-
-echo --------------------------------------------
-echo Testler Docker container icinde basariyla calistirildi.
-echo --------------------------------------------
+echo ---------------------------------------------
+echo Test Completed.
+echo ---------------------------------------------
 pause
